cfb827f13d9d544a0e56e07c2a2643f4ef0930b3
beliger <beliger@qq.com>
2018-09-19 09:56:22 +0800

    modify cxx_is_palindrome function

--- Algorithm-Design-Techniques/cxx_src/cxx_palindrome.cc
+++ Algorithm-Design-Techniques/cxx_src/cxx_palindrome.cc
@@ -8,7 +8,7 @@ int cxx_is_palindrome(string &word)
 	if (len == 0||len == 1) return 1;
 	else {
 		if (word[0] == word[len-1]) {
-			word = word.substr((len - 1), 1);
+			word = word.substr(1, len - 2);
 			return cxx_is_palindrome(word);
 		}	else return 0;
 	}
